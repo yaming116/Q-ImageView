dbb2e9bdda68621a8cf96d0bab7181936fe5c9bf
yaming116 <yaming116@163.com>
2016-06-23 13:25:44 +0800

    add image mogr2

--- library/src/main/java/me/shenfan/q/imageview/QImage.java
+++ library/src/main/java/me/shenfan/q/imageview/QImage.java
@@ -9,14 +9,28 @@ public class QImage {
 
     /** Original size for image width or height. **/
     public static final int ORIGINAL_SIZE = Integer.MIN_VALUE;
+    /** Original size for image quality. **/
+    public static final int ORIGINAL_QUALITY = Integer.MIN_VALUE;
 
     /** Image formats supported by QImageView.*/
-    public static enum ImageFormat{
+    public enum ImageFormat{
         JPG("jpg"), BMP("bmp"), GIF("gif"), PNG("png"), WEBP("webp"), YJPEG("yjpeg");
 
         final String value;
 
         ImageFormat(String value){
+            this.value = value;
+        }
+    }
+
+    public enum GravityType{
+        NorthWest("NorthWest"),North("North"),NorthEast("NorthEast")
+        ,West("West"),Center("Center"),East("East")
+        ,SouthWest("SouthWest"),South("South"),SouthEast("SouthEast");
+
+        final String value;
+
+        GravityType(String value){
             this.value = value;
         }
     }
@@ -28,5 +42,29 @@ public class QImage {
      */
     public static QImageView2UrlBuilder create(String host){
         return new QImageView2UrlBuilder(host);
+    }
+
+    /**
+     * Create a new instance for the specified host.
+     * @param host
+     * @return QImageMogr2UrlBuilder
+     */
+    public static QImageMogr2UrlBuilder createMogr(String host){
+        return new QImageMogr2UrlBuilder(host);
+    }
+
+    public static String getImageInfo(String host){
+        final String h = Utils.checkHost(host);
+        return host + "?imageInfo";
+    }
+
+    public static String getExif(String host){
+        final String h = Utils.checkHost(host);
+        return host + "?exif";
+    }
+
+    public static String getImageAve(String host){
+        final String h = Utils.checkHost(host);
+        return host + "?imageAve";
     }
 }
